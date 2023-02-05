bd78f8fb3289620ef135f9f1f05c3b070ffd6364
tjm3406 <tmill17@live.com>
2023-02-05 15:10:16 -0800

    black screen bug

--- app/src/main/java/com/example/skygazers/SecondActivity.kt
+++ app/src/main/java/com/example/skygazers/SecondActivity.kt
@@ -1,65 +1,119 @@
 package com.example.skygazers
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.skygazers.databinding.ActivitySecondBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationRequest;
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class SecondActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val viewModel: SecondActivityViewModel by viewModels()
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var viewBinding: ActivitySecondBinding
+    private final val TAG = "StarGazersApp"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewBinding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_second)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        val year = intent?.extras?.getString("year").toString().toInt()
-        val month = intent?.extras?.getString("month").toString().toInt()
-        val day = intent?.extras?.getString("day").toString().toInt()
+//        val year = intent?.extras?.getString("year").toString().toInt()
+//        val month = intent?.extras?.getString("month").toString().toInt()
+//        val day = intent?.extras?.getString("day").toString().toInt()
 
-        //get latitude and longitude
+        // get permissions
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+            Log.d("checking permissions", "requesting permissions")
             requestPermissions(
                 arrayOf(
                     Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ), 1
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.CAMERA
+                ), 100
             )
             return
         } else {
-            fusedLocationClient.requestLocationUpdates(
-                LocationRequest.create(), object :
-                    LocationCallback() {
-                    override fun onLocationResult(p0: LocationResult) {
-                        val location = p0.lastLocation
-                        viewModel.updateLatLong(location, year, month, day)
-                    }
-                },
-                Looper.getMainLooper()
-            )
+//            fusedLocationClient.requestLocationUpdates(
+//                LocationRequest.create(), object :
+//                    LocationCallback() {
+//                    override fun onLocationResult(p0: LocationResult) {
+//                        val location = p0.lastLocation
+//                        viewModel.updateLatLong(location, year, month, day)
+//                    }
+//                },
+//                Looper.getMainLooper()
+//            )
+            startCamera()
         }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -68,9 +122,9 @@ class SecondActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        val year = intent?.extras?.getString("year").toString().toInt()
-        val month = intent?.extras?.getString("month").toString().toInt()
-        val day = intent?.extras?.getString("day").toString().toInt()
+//        val year = intent?.extras?.getString("year").toString().toInt()
+//        val month = intent?.extras?.getString("month").toString().toInt()
+//        val day = intent?.extras?.getString("day").toString().toInt()
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -78,19 +132,29 @@ class SecondActivity : AppCompatActivity() {
             ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            fusedLocationClient.requestLocationUpdates(
-                LocationRequest.create(), object :
-                    LocationCallback() {
-                    override fun onLocationResult(p0: LocationResult) {
-                        val location = p0.lastLocation
-                        viewModel.updateLatLong(location, year, month, day)
-                    }
-                },
-                Looper.getMainLooper()
-            )
+
+            startCamera()
+//            fusedLocationClient.requestLocationUpdates(
+//                LocationRequest.create(), object :
+//                    LocationCallback() {
+//                    override fun onLocationResult(p0: LocationResult) {
+//                        val location = p0.lastLocation
+//                        viewModel.updateLatLong(location, year, month, day)
+//                    }
+//                },
+//                Looper.getMainLooper()
+//            )
         }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
